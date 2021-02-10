4ef0ec318ba286c71472a7bf30c0fb88d39b380e
soumya prakash <50635298+soumya-05@users.noreply.github.com>
2021-02-10 19:58:41 +0530

    soumya in alumni

--- frontend/src/components/Alumini.js
+++ frontend/src/components/Alumini.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const alumini = () => {
-    return (
-        <div>
-            <h1>I am future Alumini page</h1>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>I am future Alumini page by soumya</h1>
+    </div>
+  )
 }
 
 export default alumini
