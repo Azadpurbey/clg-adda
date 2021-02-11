63e2c4eb39abe33d6a25a4f852b4b5198b6785ec
Sahil Seli <sahilseli7236@gmail.com>
2021-02-11 13:57:34 +0530

    db changes

---
+++ config/db.js
@@ -0,0 +1,17 @@
+
+import mongoose from 'mongoose'
+
+
+const connectDb=async ()=>{
+    try{
+
+        const conn=await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true});
+        console.log(`Database is connected`)
+           
+         
+    }catch(Error){
+            console.log('error : ',Error);
+    }
+}
+
+export default connectDb;
---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+
+import express from 'express'
+const router = express.Router();
+
+import {signup,signin,otpController} from '../controllers/auth.js'
+import {authToken} from '../middleware/decodeToken.js'
+
+
+router.post('/otp',otpController);
+router.post('/signup',signup);
+router.post('/signin',signin);
+// router.get('/',authToken,(req,res)=>res.send(req.user._id));
+
+export default router;
