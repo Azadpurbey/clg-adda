84e96852df127771c3112b64bfcc0c4c721b74ea
Azad Kumar <azadpurbey2@gmail.com>
2021-04-02 18:25:14 +0530

    bug fixes

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose'
 
+const otpSchema = new mongoose.Schema({
+  createdAt: { type: Date, expires: '180s', default: Date.now },
+  otp: {
+    type: Number,
+    required: true,
+  },
+})
+// otpSchema.createIndex( { "lastModifiedDate": 1 }, { expireAfterSeconds: 30 } )
 
-const otpSchema=new mongoose.Schema({
-      createdAt: { type: Date, expires: '60s',  default: Date.now },
-        otp:{
-            type:Number,
-            required:true
-        }       
-    })
-    // otpSchema.createIndex( { "lastModifiedDate": 1 }, { expireAfterSeconds: 30 } )
-
-    const OTP = mongoose.model('OTP',otpSchema);
-    export default OTP;
-
+const OTP = mongoose.model('OTP', otpSchema)
+export default OTP
