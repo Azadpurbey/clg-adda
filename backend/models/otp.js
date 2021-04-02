23286f2ec6392c260076bc8188eff3a9b598eaeb
Sahil Seli <sahilseli7236@gmail.com>
2021-04-02 14:54:12 +0530

    otp-model changed

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 
 
 const otpSchema=new mongoose.Schema({
+      createdAt: { type: Date, expires: '60s',  default: Date.now },
         otp:{
             type:Number,
             required:true
         }       
-    },{ timestamps: true })
+    })
+    // otpSchema.createIndex( { "lastModifiedDate": 1 }, { expireAfterSeconds: 30 } )
 
     const OTP = mongoose.model('OTP',otpSchema);
     export default OTP;
